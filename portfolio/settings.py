daf6300cf25d1170643ad9ef839be57eb16e85b3
babar.spyresync <babar.spyresync@gmail.com>
2022-10-10 18:18:39 +0500

    update domain

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'portfolio-production-8644.up.railway.app', 'baberibrar.me', 'www.baberibrar.me']
 
 # Application definition
 
