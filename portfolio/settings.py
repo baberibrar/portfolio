9ee087c76431f6e9212069a7bcfd59b7c98c6e95
babar.spyresync <babar.spyresync@gmail.com>
2022-10-11 12:15:26 +0500

    Deploy on Heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -112,7 +112,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 
